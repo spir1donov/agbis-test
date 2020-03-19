044962507f04605a7b51b1cd5ffe67f00d78d54a
Nikita <slxxxix@gmail.com>
2020-03-19 19:18:09 +0200

    add basic routes: login, logout and orders listing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import withAuth from './middleware/withAuth'
+import Orders from './views/Orders'
+import Login from './views/Login'
+import Logout from './views/Logout'
+import './App.scss'
 
-function App() {
+const App = () => {
   return (
-    <div/>
-  );
+    <div>
+      <Switch>
+        <Route path="/" exact component={withAuth(Orders)} />
+        <Route path="/logout" component={withAuth(Logout)} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </div>
+  )
 }
 
-export default App;
+export default App
